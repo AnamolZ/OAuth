4f803fb47e15ed09278a57ff8b2f16029b77ab28
Anamol <anmoldkl971@gmail.com>
2023-11-25 15:00:57 +0545

    version 1.0

--- utilities/func.py
+++ utilities/func.py
@@ -27,16 +27,3 @@ def hash_password(password):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
-
-def get_user(username: str):
-    if username in db_users:
-        user_dict = db_users[username]
-        return User(**user_dict)
-
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
-    if not user:
-        return False
-    if not verify_password(password, user.password):
-        return False
-    return user
