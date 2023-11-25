f8f9d3f5bc7d96d787dd2cc8b04fc3a128209494
Anamol <anmoldkl971@gmail.com>
2023-11-25 14:52:24 +0545

    version 1.0

--- dataBase/credentialBase.py
+++ dataBase/credentialBase.py
@@ -1,4 +1,9 @@
-db_users = {
-    "username": "admin",
-    "password": "admin"
+db = {
+    "john": {
+        "username": "john",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$FmBRKfDs0iu0uO4YPkKf1eeWlkVWyzArwhNcQxp8bVK7kAELBmT4a",
+        "disabled": False,
+    }
 }
