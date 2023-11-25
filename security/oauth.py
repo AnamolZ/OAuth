6569b5cd7feb74e9abb6bba0a174cca43ecf8019
Anamol <anmoldkl971@gmail.com>
2023-11-25 14:45:33 +0545

    version 1.0

--- security/oauth.py
+++ security/oauth.py
@@ -23,8 +23,8 @@ class User(BaseModel):
 
 db = {
     "john": {
-        "username": "john",
-        "full_name": "John Doe",
+        "username": "Xznom",
+        "full_name": "Xznom Doe",
         "email": "johndoe@example.com",
         "hashed_password": "$2b$12$FmBRKfDs0iu0uO4YPkKf1eeWlkVWyzArwhNcQxp8bVK7kAELBmT4a",
         "disabled": False,
