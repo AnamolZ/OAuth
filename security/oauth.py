4505b787e2db8d429a07dfeae28f694bc01a35c7
Anamol <anmoldkl971@gmail.com>
2023-11-25 15:13:49 +0545

    version 1.0

--- security/oauth.py
+++ security/oauth.py
@@ -8,7 +8,7 @@ from passlib.context import CryptContext
 
 app = FastAPI()
 
-SECRET_KEY = "b"
+SECRET_KEY = "bdWIiOiJ4em5vbSIsImV4cCI6MTcwMDk"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
